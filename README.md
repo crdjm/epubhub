7d0f68f29ffe0e2a304d3e4e322031cd96530bdb
David Metcalfe <crdjm1@gmail.com>
2024-11-23 14:12:40 -0600

    Update how cheerio is used to remove typescript errors

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 > This project is a work in progress. It is not ready for use yet.
 > It is a fork of the epub-modify project that appears to be no longer maintained
-> Saved epubs from epub-modiy were were not valid to epubcheck due to
-> compression of the mimetype file. In fixing that, I realised there as more
+> Saved epubs from epub-modiy were not valid to epubcheck due to
+> compression of the mimetype file. In fixing that, I realised there is more
 > functionality I wanted to add in support of various epub related projects,
 > hence my forking this. I am submitting this now just to see if I have a handle on
 > how to create and publish an npm package. More to come...
--- src/parseNav.ts
+++ src/parseNav.ts
@@ -1,40 +1,39 @@
-import cheerio from 'cheerio'
-import { xml2obj } from './utils'
+const cheerio = require("cheerio");
+import { xml2obj } from "./utils";
 
 export function parseNav(html): any {
-  const $ = cheerio.load(html)
-
-  const nav = {}
-
-  $('nav').each((i, elem) => {
-    const $nav = $(elem)
-    const type = $nav.attr('epub:type')
-
-    nav[type] = $nav.find('ol > li').map((j, li) => {
-      const $elem = $(li)
-      return {
-        title: $elem.find('a').text(),
-        href: $elem.find('a').attr('href'),
-      }
-    }).get()
-  })
-
-  return nav
+  const $ = cheerio.load(html);
+  const nav = {};
+  $("nav").each((i, elem) => {
+    const $nav = $(elem);
+    const type = $nav.attr("epub:type");
+    nav[type] = $nav
+      .find("ol > li")
+      .map((j, li) => {
+        const $elem = $(li);
+        return {
+          title: $elem.find("a").text(),
+          href: $elem.find("a").attr("href"),
+        };
+      })
+      .get();
+  });
+  return nav;
 }
 
 export function parseNcx(xml): any {
-  const ncxObj = xml2obj(xml)
+  const ncxObj = xml2obj(xml);
 
-  if (!ncxObj.ncx.navMap || !ncxObj.ncx.navMap.navPoint) return []
+  if (!ncxObj.ncx.navMap || !ncxObj.ncx.navMap.navPoint) return [];
 
   const navPoints = Array.isArray(ncxObj.ncx.navMap.navPoint)
     ? ncxObj.ncx.navMap.navPoint
-    : [ncxObj.ncx.navMap.navPoint]
+    : [ncxObj.ncx.navMap.navPoint];
 
-  return navPoints.map(navPoint => {
+  return navPoints.map((navPoint) => {
     return {
       title: navPoint.navLabel.text,
       href: navPoint.content.attr.src,
-    }
-  })
+    };
+  });
 }
